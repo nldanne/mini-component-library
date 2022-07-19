a1648c10aacad2b2f3952696c96b83db144b0295
nldanne <80318612+nldanne@users.noreply.github.com>
2022-07-19 17:14:53 +0200

    finished searchbar

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,87 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24
+  },
+
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36
+  }
+};
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+
+  const styles = STYLES[size];
+
+  if(!size) {
+    throw new Error(`Unknown size was passed to IconInput: ${size}`)
+  }
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }} >
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      {/* collect additional props suhc as placeholder coming with 'Input' component */}
+      <Input {...delegated} style={{ 
+          '--width': width / 16 + 'rem',
+          '--height': styles.height / 16 + 'rem',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize / 16 + 'rem'
+     }}/>
+    </Wrapper>
+  );
 };
+
+/* ALWAYS wrap label element around an input tag */
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const Input = styled.input`
+  font-size: var(--fontSize);
+  height: var(--height);
+  width: var(--width);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
